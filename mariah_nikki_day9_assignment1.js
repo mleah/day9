2330414fa640d94be834fbaba5945143524d4777
mleah <mariah.hanson@detroitlabs.com>
2015-03-05 14:09:06 -0500

    adding skeletal code structure

--- mariah_nikki_day9_assignment1.js
+++ mariah_nikki_day9_assignment1.js
@@ -4,6 +4,8 @@ sget for userinput
 quit function
 loops through the switch/case until user quits  */
 
+var sget = require('sget');
+
 
 
 function addition(){
@@ -26,3 +28,50 @@ function squareRoot(){
 
 }
 
+function quitCalculator(){
+
+}
+
+function startCalculator(){
+
+}
+
+function calculator(){
+	var calcChoice = sget("");
+	calcChoice = parseInt(calcChoice);
+		switch(itemChoice){
+			case 1:
+				console.log("")
+				
+				break;
+
+			case 2:
+				console.log("");
+				
+				break;
+
+			case 3:
+				console.log("");
+				
+				break;
+
+			case 4:
+				
+					console.log("");
+			
+				break;
+
+			case 5:
+				console.log("");
+				
+				break;
+
+			case 6:
+				console.log("")
+			
+				break;
+
+			default: 
+				console.log("");
+		}
+}
