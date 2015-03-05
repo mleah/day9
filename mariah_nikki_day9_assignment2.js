94767e4df7bf696f9474b980b1a1f2b4322098c6
mleah <mariah.hanson@detroitlabs.com>
2015-03-05 17:23:01 -0500

    adding isPalindrome function, comparison is not working.  Debugging.

--- mariah_nikki_day9_assignment2.js
+++ mariah_nikki_day9_assignment2.js
@@ -7,13 +7,31 @@ console log if the word is or isn'ta palindrome
 */
 
 var sget = require("sget");
+chooseWord();
 
-
-function chooseWord {
-	console.log("Please type a word.  This program will check to see if the word is a palindrome.");
-	
+function chooseWord(){
+	var chosenWord = sget("Please type a word.  This program will check to see if the word is a palindrome.");
+	reverseWord(chosenWord);
 }
 
 function reverseWord(chosenWord) {
   var reversedWord = chosenWord.split('').reverse().join('');
+  console.log(chosenWord);
+  console.log(reversedWord);
+  console.log(typeof chosenWord);
+  console.log(typeof reversedWord);
+  isPalindrome(chosenWord, reversedWord);
+
+}
+
+function isPalindrome(chosenWord, reversedWord){
+	console.log(chosenWord);
+	console.log(reversedWord);
+
+	if (chosenWord == reversedWord){
+		console.log(chosenWord + " is a palindrome!");
+	}
+	else{
+		console.log(chosenWord + " is not a palindrome!");
+	}
 }
