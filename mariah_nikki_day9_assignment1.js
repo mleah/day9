03e082b4404c9ed9295b7d3dea376651ab4b2c81
mleah <mariah.hanson@detroitlabs.com>
2015-03-05 13:51:04 -0500

    adding assignment 1

---
+++ mariah_nikki_day9_assignment1.js
@@ -0,0 +1,28 @@
+/*  switch/case  for all of the options
+functions for each case/option
+sget for userinput
+quit function
+loops through the switch/case until user quits  */
+
+
+
+function addition(){
+
+}
+
+function subtraction(){
+
+}
+
+function multiplication(){
+
+}
+
+function division(){
+
+}
+
+function squareRoot(){
+
+}
+
