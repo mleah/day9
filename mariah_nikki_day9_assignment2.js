87ed8f619ac16eea7cb2ed4784f93d34a4cc9ee8
mleah <mariah.hanson@detroitlabs.com>
2015-03-05 16:39:07 -0500

    adding some initial function ideas for reversing the word

--- mariah_nikki_day9_assignment2.js
+++ mariah_nikki_day9_assignment2.js
@@ -1,5 +1,19 @@
 /*
 PSEUDOCODE
-
-
+we need to take a word with sget
+change it so it is backwards
+check to see if its the same forwards and backwards
+console log if the word is or isn'ta palindrome
 */
+
+var sget = require("sget");
+
+
+function chooseWord {
+	console.log("Please type a word.  This program will check to see if the word is a palindrome.");
+	
+}
+
+function reverseWord(chosenWord) {
+  var reversedWord = chosenWord.split('').reverse().join('');
+}
