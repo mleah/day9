db12d3e50ad4607cea791eeee314ce3ba92364f4
mleah <mariah.hanson@detroitlabs.com>
2015-03-06 13:44:45 -0500

    fixing bug so that spaces and special characters don't break it

--- mariah_nikki_day9_assignment2.js
+++ mariah_nikki_day9_assignment2.js
@@ -4,13 +4,16 @@ we need to take a word with sget
 change it so it is backwards
 check to see if its the same forwards and backwards
 console log if the word is or isn'ta palindrome
+
+HOW DO WE MAKE IT SO THAT SPACED WORDS LIKE TACO CAT CANT BREAK IT?!?!
 */
 
 var sget = require("sget");
 chooseWord();
 
 function chooseWord(){
-	var chosenWord = sget("Please type a word.  This program will check to see if the word is a palindrome.").trim();
+	var chosenWord = sget("Please type a word.  This program will check to see if the word is a palindrome.").trim().replace(/\W/g, "").toLowerCase();
+	console.log(chosenWord);
 	reverseWord(chosenWord);
 }
 
